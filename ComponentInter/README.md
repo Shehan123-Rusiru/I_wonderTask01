e8cfa21d52014970af004484f8090ac5cede983a
Shehan123-Rusiru <ss.rusiru@gmail.com>
2023-01-05 09:53:19 +0530

    new project first commit

--- ComponentInter/README.md
+++ ComponentInter/README.md
@@ -1,4 +1,4 @@
-# Task01
+# ComponentInter
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.4.
 
--- ComponentInter/src/app/app.component.spec.ts
+++ ComponentInter/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Task01'`, () => {
+  it(`should have as title 'ComponentInter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Task01');
+    expect(app.title).toEqual('ComponentInter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Task01 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ComponentInter app is running!');
   });
 });
